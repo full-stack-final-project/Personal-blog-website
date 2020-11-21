27e3e58599c86a31c7ee19e1ff33a50ebdb05a1c
Yihua <yihua@pdx.edu>
2020-11-20 21:02:41 -0800

    form class

---
+++ blog/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField, ValidationError
+from wtforms.validators import DataRequired, Length, Email
+from flask_ckeditor import CKEditorField
+from blog.models import Category
+
+class login_form(FlaskForm):
+    username = StringField('Username', validators = [DataRequired(), Length(1, 30)])
+    password = PasswordField('Password', validators = [DataRequired(), Length(8, 128)])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Log in')
+
+class article_form(FlaskForm):
+    title = StringField('Title', validators = [DataRequired(), Length(1, 50)])
+    category = SelectField('Category', coerce = int, default = 1)
+    body = CKEditorField('Body', validators = [DataRequired()])
+    submit = SubmitField()
+
+    def __init__(self, *args, **kwargs):
+        super(article_form, self).__init__(*args, **kwargs)
+        self.category.choices = [(category.id, category.name) for category in Category.query.order_by(Category.name).all()]
+class category_form(FlaskForm):
+    name = StringField('Name', validators = [DataRequired, Length(1, 30)])
+    submit = SubmitField()
+
+    def validate_name(self, field):
+        if Category.query.filter_by(name = field.data).first():
+            raise ValidationError('Name already in use')
+
+class comment_form(FlaskForm):
+    person_post = StringField('Name', validators = [DataRequired(), Length(1, 30)])
+    email = StringField('Email', validators = [DataRequired(), Email(), Length(1, 255)])
+    site = StringField('Site', validators = [Optional(), URL(), Length(0, 255)])
+    body = TextAreaField('Comment', validators = [DataRequired()])
+    submit = SubmitField()
