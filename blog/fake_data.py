9dcd8cddd9ba3f528d8146ae8d3c668d608cc6ba
Yihua <yihua@pdx.edu>
2020-11-23 18:48:28 -0800

    add file for faking data

---
+++ blog/fake_data.py
@@ -0,0 +1,60 @@
+from faker import Faker 
+from blog.extensions import db
+from blog.models import Admin, Category, Comment, Article
+from sqlalchemy.exc import IntegrityError
+
+import random
+
+fake = Faker()
+
+# fake admin data
+def fake_admin():
+    admin = Admin(
+        username = 'admin',
+        name = fake.name(),
+        about = 'This is my first personal blog which is created by Junhao and Yihua. \
+                    I will share my stories and some articles in this blog.',
+        site_title = name + "'s Personal blog"
+    )
+    admin.set_password('20fallweb')
+    db.session.add(admin)
+    db.session.commit()
+
+# fake categories data
+def fake_categories(count = 10):
+    for i in range(count):
+        category = Category(name = fake.work())
+        db.session.add(category)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+
+# fake articles data
+def fake_articles(count = 100):
+    for i in range(count):
+        article = Article(
+            title = fake.sentence(),
+            body = fake.text(3000),
+            category = Category.query.get(random.randint(1, Category.query.count())),
+            timestamp = fake.data_time_this_year()
+        )
+        db.session.add(article)
+    db.session.commit()
+
+# fake comments data
+def fake_comments(count = 300):
+    for i in range(count):
+        comment = Comment(
+            person_post = fake.name(),
+            body = fake.sentence(),
+            reviewed = True,
+            timestamp = fake.data_time_this_year(),
+            email = fake.email(),
+            site = fake.url(),
+            article = Article.query.get(random.randint(1, Article.query.count()))
+        )
+        db.session.add(comment)
+    db.session.commit()
+
+    
